31ab3b6340f50c03d079abad2e119ae6346d6b5d
Andre Kirchner <kirchnerandre@hotmail.com>
2025-02-06 19:22:57 -0800

    Add function to get vertices, valid edges, and plot them

--- 01/project_01b.m
+++ 01/project_01b.m
@@ -5,10 +5,15 @@ clc
 
 tic
 
-start       = [4,  4 ];
-goal        = [90, 85];
-n           = 0;
-circleSize  = 2;
+vertex_initial  = [4,  4 ];
+vertex_final    = [90, 85];
+n               = 0;
+circleSize      = 2;
+
+obstacles(:,:,1) = [4 0; 20 10; 20 30; 60 30; 60 10];
+obstacles(:,:,2) = [4 0; 70 10; 70 50; 90 50; 90 10];
+obstacles(:,:,3) = [4 0; 10 40; 10 60; 50 60; 50 40];
+obstacles(:,:,4) = [4 0; 20 70; 20 90; 80 90; 80 70];
 
 figure()
 
@@ -16,50 +21,42 @@ axis([0 100 0 100])
 axis square
 hold on
 
-obs(:,:,1) = [4 0; 20 10; 20 40; 60 40; 60 10];
-obs(:,:,2) = [4 0; 50 60; 50 80; 70 80; 70 60];
+circles(vertex_initial(1), vertex_initial(2), circleSize, 'facecolor', 'green')
+circles(vertex_final  (1), vertex_final  (2), circleSize, 'facecolor', 'yellow')
 
-n = size(obs,3);
+n = size(obstacles, 3);
 
-for i = 1:n
-    numVert = obs(1,1,i);
-    pgon    = polyshape(obs(2:numVert+1,:,i));
-    plot(pgon);
-    
-    if exist('origObs') == 1
-        numVert = origObs(1,1,i);
-        pgon = polyshape(origObs(2:numVert+1,:,i));
-        plot(pgon);
-    end
-end
+plot_obstacles(obstacles)
 
-obsx = cell(n,1);
-obsy = cell(n,1);
+vertices    = get_vertices(vertex_initial, vertex_final, obstacles);
+edges       = get_egdes(vertices, obstacles);
+
+plot_edges(edges, vertices);
+
+obstaclesx = cell(n,1);
+obstaclesy = cell(n,1);
 
 for i = 1:n
-    numVert = obs(1,1,i);
-    curObs = obs(2:numVert+1,:,i);
-    obsx(i) = {curObs(:,1)'};
-    obsy(i) = {curObs(:,2)'};
+    numVert         = obstacles(1,1,i);
+    curObs          = obstacles(2:numVert+1,:,i);
+    obstaclesx(i)   = {curObs(:,1)'};
+    obstaclesy(i)   = {curObs(:,2)'};
 end
-
-circles(start(1), start(2),circleSize, 'facecolor', 'green')
-circles(goal(1),  goal(2), circleSize, 'facecolor', 'yellow')
 
 keys    = {1};
-values  = {start};
+values  = {vertex_initial};
 index   = 1;
 
 for i = 0:n-1
-    for j = 2:obs(1,1,i+1)+1
+    for j = 2:obstacles(1,1,i+1)+1
         keys  (1,index+1) = {index+1};
-        values(1,index+1) = {obs(j,:,i+1)};
+        values(1,index+1) = {obstacles(j,:,i+1)};
         index = index + 1;
     end
 end
 
 keys  (end+1) = {index};
-values(end+1) = {goal};
+values(end+1) = {vertex_final};
 
 Map     = containers.Map(keys, values);
 len     = Map.values;
@@ -95,7 +92,7 @@ for i=1:sizeEd(1)
     notInObs = true;
     
     for k = 1:n
-        [in,on] = inpolygon(xpoints, ypoints,obsx{k},obsy{k});
+        [in,on] = inpolygon(xpoints, ypoints,obstaclesx{k},obstaclesy{k});
         
         if max(xor(in,on))
             notInObs = false;
@@ -131,3 +128,73 @@ end
 time = toc;
 disp("Environment 1 " + ": Time = " + time + " sec, Distance = " + totalDis);
 xlabel({"Environment 1 "; "Time = " + time + " sec"; "Distance = " + totalDis});
+
+function plot_obstacles(Obstacles)
+    for i = 1:size(Obstacles, 3)
+        numVert = size(Obstacles, 3)
+        pgon    = polyshape(Obstacles(2:numVert+1,:,i));
+        plot(pgon);
+    end
+end
+
+function plot_edges(Edges, Vertices)
+    for i = 1:size(Edges, 1)
+        plot([Vertices(Edges(i, 1), 1) Vertices(Edges(i, 2), 1)], ...
+             [Vertices(Edges(i, 1), 2) Vertices(Edges(i, 2), 2)])
+    end
+end
+
+function Vertices = get_vertices(VertexInitial, VertexFinal, Obstacles)
+    vertices_size = 2; % VertexInitial + VertexFinal 
+    
+    for i = 1:size(Obstacles, 3)
+        vertices_size = vertices_size + Obstacles(1, 1, i);
+    end
+    
+    Vertices = zeros(vertices_size, 2);
+    
+    Vertices(vertices_size, :) = VertexFinal;
+    
+    for i = 1:size(Obstacles, 3)
+        for j = 2:Obstacles(1, 1, i)+1
+            vertices_size = vertices_size - 1;
+            Vertices(vertices_size, :) = Obstacles(j, :, i);
+        end
+    end
+    
+    Vertices(1, :) = VertexInitial;
+end
+
+function Edges = get_egdes(Vertices, Obstacles)
+    edges_size  = factorial(size(Vertices, 1)) / factorial(size(Vertices, 1) - 2) / 2;
+    Edges       = zeros(edges_size, 3);
+    index       = 0;
+
+    for i = 1:size(Vertices, 1)
+        for j = (i + 1):size(Vertices, 1)
+            points = 4 * max(abs(Vertices(i, 1) - Vertices(j, 1)), ...
+                             abs(Vertices(i, 2) - Vertices(j, 2)));
+
+            points_x = linspace(Vertices(i, 1), Vertices(j, 1), points);
+            points_y = linspace(Vertices(i, 2), Vertices(j, 2), points);
+
+            valid = true;
+
+            for k = 1:size(Obstacles, 3)
+                [ in, on ] = inpolygon(points_x, points_y, Obstacles(2:end, 1, k), Obstacles(2:end, 2, k));
+
+                if max(xor(in, on))
+                    valid = false;
+                    break
+                end
+            end
+
+            if valid
+                index = index + 1;
+                Edges(index, :) = [i j sqrt((Vertices(i, 1) - Vertices(j, 1)) ^ 2 + (Vertices(i, 2) - Vertices(j, 2)) ^ 2)];
+            end
+        end
+    end
+
+    Edges = Edges(1:index, :);
+end
