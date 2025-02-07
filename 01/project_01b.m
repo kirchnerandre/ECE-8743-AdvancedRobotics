e1bd9c520cd9c114a68b6070b4a62021b2fe7317
Andre Kirchner <kirchnerandre@hotmail.com>
2025-02-06 20:05:26 -0800

    Better encapsulate code

--- 01/project_01b.m
+++ 01/project_01b.m
@@ -28,10 +28,26 @@ n = size(obstacles, 3);
 
 plot_obstacles(obstacles)
 
-vertices    = get_vertices(vertex_initial, vertex_final, obstacles);
-edges       = get_egdes(vertices, obstacles);
+vertices    = get_vertices(vertex_initial, vertex_final, obstacles)
+edges       = get_egdes(vertices, obstacles)
 
 plot_edges(edges, vertices);
+
+path = get_path(vertices, edges);
+
+plot_path(path, vertices)
+
+
+figure()
+
+axis([0 100 0 100])
+axis square
+hold on
+
+plot_obstacles(obstacles)
+
+circles(vertex_initial(1), vertex_initial(2), circleSize, 'facecolor', 'green')
+circles(vertex_final  (1), vertex_final  (2), circleSize, 'facecolor', 'yellow')
 
 obstaclesx = cell(n,1);
 obstaclesy = cell(n,1);
@@ -126,8 +142,8 @@ for i=1:pathSize(2)-1
 end
 
 time = toc;
+
 disp("Environment 1 " + ": Time = " + time + " sec, Distance = " + totalDis);
-xlabel({"Environment 1 "; "Time = " + time + " sec"; "Distance = " + totalDis});
 
 function plot_obstacles(Obstacles)
     for i = 1:size(Obstacles, 3)
@@ -140,7 +156,14 @@ end
 function plot_edges(Edges, Vertices)
     for i = 1:size(Edges, 1)
         plot([Vertices(Edges(i, 1), 1) Vertices(Edges(i, 2), 1)], ...
-             [Vertices(Edges(i, 1), 2) Vertices(Edges(i, 2), 2)])
+             [Vertices(Edges(i, 1), 2) Vertices(Edges(i, 2), 2)], 'b')
+    end
+end
+
+function plot_path(Path, Vertices)
+    for i = 1:(size(Path, 2) - 1)
+        plot([Vertices(Path(i), 1) Vertices(Path(i + 1), 1)], ...
+             [Vertices(Path(i), 2) Vertices(Path(i + 1), 2)], 'k', 'LineWidth', 3)
     end
 end
 
@@ -197,4 +220,24 @@ function Edges = get_egdes(Vertices, Obstacles)
     end
 
     Edges = Edges(1:index, :);
+end
+
+function Path = get_path(Vertices, Edges)
+    sources = zeros(2 * size(Edges, 1), 1);
+    targets = zeros(2 * size(Edges, 1), 1);
+    weights = zeros(2 * size(Edges, 1), 1);
+
+    for i = 1:size(Edges, 1)
+        sources(2 * i - 1)  = Edges(i, 1);
+        targets(2 * i - 1)  = Edges(i, 2);
+        weights(2 * i - 1)  = Edges(i, 3);
+
+        sources(2 * i)      = Edges(i, 2);
+        targets(2 * i)      = Edges(i, 1);
+        weights(2 * i)      = Edges(i, 3);
+    end
+
+    G = graph(sources, targets, weights);
+
+    Path = shortestpath(G, 1, size(Vertices, 1));
 end
