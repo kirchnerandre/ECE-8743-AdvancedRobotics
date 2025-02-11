f95dde2c69eac8715eb0331aed07f98fb926c131
Andre Kirchner <kirchnerandre@hotmail.com>
2025-02-10 17:42:15 -0800

    Move non-critical code to main function

--- 01/visibility_graph.m
+++ 01/visibility_graph.m
@@ -1,16 +1,12 @@
-function [ Time Distance ] = visibility_graph(VertexInitial, VertexFinal, Obstacles)
-    tic
+function [ Distance Path Edges Vertices ] = visibility_graph(VertexInitial, VertexFinal, Obstacles)
+    Vertices    = get_vertices(VertexInitial, VertexFinal, Obstacles);
+    Edges       = get_egdes(Vertices, Obstacles);
 
-    vertices    = get_vertices(VertexInitial, VertexFinal, Obstacles);
-    edges       = get_egdes(vertices, Obstacles);
-    
-    [ path Distance ] = get_path(vertices, edges);
-    
+    [ Path Distance ] = get_path(Vertices, Edges);
+
     plot_obstacles(Obstacles)
-    
-    plot_edges(edges, vertices)
-    
-    plot_path(path,  vertices)
-    
-    Time = toc;
+
+    plot_edges(Edges, Vertices)
+
+    plot_path(Path,  Vertices)
 end
