bc4f7f95419ba9997f1451a4ed036dc31a9eafa6
Andre Kirchner <kirchnerandre@hotmail.com>
2025-02-12 20:01:11 -0800

    Almost done

---
+++ 01/get_tangents_non.m
@@ -0,0 +1,47 @@
+function Edges = get_tangents_non(Vertices, Obstacles)
+    length  = sum(Obstacles(1, 1, :)) ...
+            + sum(Obstacles(1, 1, :)) ...
+            + sum(Obstacles(1, 1, :));
+
+    Edges   = zeros(length, 3);
+    initial = 1;
+    final   = size(Vertices, 1);
+    index   = 0;
+    offset  = 1;
+
+    for i = 1:size(Obstacles, 3)
+        for j = 1:Obstacles(1, 1, i)
+            index           = index + 1;
+            Edges(index, :) = [ initial (j + offset) 0 ];
+        end
+
+        offset = offset + Obstacles(1, 1, i);
+    end
+
+    offset  = 1;
+
+    for i = 1:size(Obstacles, 3)
+        for j = 1:Obstacles(1, 1, i)
+            index           = index + 1;
+            Edges(index, :) = [ final   (j + offset) 0 ];
+        end
+
+        offset = offset + Obstacles(1, 1, i);
+    end
+
+    offset  = 1;
+
+    for i = 1:size(Obstacles, 3)
+        for j = 1:Obstacles(1, 1, i)
+            index               = index + 1;
+
+            if j ~= 1
+                Edges(index, :) = [ (j + offset - 1)    (j + offset)                    0 ];
+            else
+                Edges(index, :) = [ (1 + offset)        (Obstacles(1, 1, i) + offset)   0 ];
+            end
+        end
+
+        offset = offset + Obstacles(1, 1, i);
+    end
+end
--- 01/visibility_graph_tangent.m
+++ 01/visibility_graph_tangent.m
@@ -3,21 +3,8 @@ function [ Distance Path Edges Vertices ] = visibility_graph_tangent(VertexIniti
     Path = [];
 
     Vertices    = get_vertices(VertexInitial, VertexFinal, Obstacles);
-    Edges       = get_tangents_all(Vertices, Obstacles);
-
-    figure
-    axis([ -2 18 -2 18 ]);
-    axis square;
-    hold on
-    plot_obstacles(Obstacles)
-    plot_edges(Edges, Vertices)
+    Edges       = [ get_tangents_all(Vertices, Obstacles); ...
+                    get_tangents_non(Vertices, Obstacles); ];
 
     Edges       = clean_edges(Edges, Vertices, Obstacles);
-    
-    figure
-    axis([ -2 18 -2 18 ]);
-    axis square;
-    hold on
-    plot_obstacles(Obstacles)
-    plot_edges(Edges, Vertices)
 end
