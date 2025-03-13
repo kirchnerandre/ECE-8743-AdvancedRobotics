2dd6d473597a5fc0b8f90cfb53690415d25b8849
Andre Kirchner <kirchnerandre@hotmail.com>
2025-03-13 14:44:26 -0700

    Add timer

--- Project-02/main.m
+++ Project-02/main.m
@@ -1,17 +1,17 @@
-function main(PositionBegin, ...
-              PositionEnd, ...
-              SensorRange, ...
-              StepSize, ...
-              Clearance)
+function Time = main(PositionBegin, ...
+                     PositionEnd, ...
+                     SensorRange, ...
+                     StepSize, ...
+                     Clearance)
     position_middle = PositionBegin;
     left_bottom     = [min(PositionBegin(1), PositionEnd(1)) ...
                        min(PositionBegin(2), PositionEnd(2))];
     right_top       = [max(PositionBegin(1), PositionEnd(1)) ...
                        max(PositionBegin(2), PositionEnd(2))];
-    
+
     [ obstacles_data, obstacles_length ] = create_obstacles(PositionBegin, ...
                                                             PositionEnd);
-    
+
     figure;
     axis([ left_bottom(1) - SensorRange, right_top(1) + SensorRange, ...
            left_bottom(2) - SensorRange, right_top(2) + SensorRange ]);
@@ -19,42 +19,46 @@ function main(PositionBegin, ...
     set(gca,'XLimMode','manual');
     set(gca,'YLimMode','manual');
     hold on
-    
+
     plot_obstacles(obstacles_data, obstacles_length);
-    
+
     plot(PositionBegin(1), PositionBegin(2), 'r+', "LineWidth", 2, "MarkerSize", 5)
     plot(PositionEnd(1),   PositionEnd(2),   'r*', "LineWidth", 2, "MarkerSize", 5)
-    
+
     position_begin = PositionBegin;
+
+    tic
 
     while true
         radar_data = compute_radar(obstacles_data, ...
                                    obstacles_length, ...
                                    position_begin, ...
                                    SensorRange);
-    
+
         radar_data = compute_clearance(radar_data, Clearance);
-    
+
         [ position_middle position_begin ] = compute_step(position_begin, ...
                                                           PositionEnd, ...
                                                           radar_data, ...
                                                           StepSize);
-    
+
         plot_robot(position_begin)
-    
+
         plot_path(position_begin, position_middle, PositionEnd)
-    
+
         plot_radar_range(position_begin, SensorRange, 1.0)
-    
+
         plot_radar_detection(position_begin, radar_data, 1.0)
-    
+
         distance = sqrt((PositionEnd(1) - position_begin(1)) ^ 2 ...
                       + (PositionEnd(2) - position_begin(2)) ^ 2);
-    
+
         if distance < StepSize
             break
         end
-    
+
         pause(0.01)
     end
+
+    Time = toc;
 end
--- Project-02/tester.m
+++ Project-02/tester.m
@@ -4,9 +4,9 @@ clc
 
 position_begin  = [ 3.00; 3.00 ];
 position_end    = [ 5.00; 6.00 ];
-clearance       = 10;
-step_size       = 0.01;
 sensor_range    = 0.5;
+step_size       = 0.01;
+clearance       = 10;
 
 main(position_begin, ...
      position_end, ...
