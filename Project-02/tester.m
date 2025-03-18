52db3460b95ee8575396204de81e9dbb249bea55
Andre Kirchner <kirchnerandre@hotmail.com>
2025-03-17 17:45:03 -0700

    Fix bug

--- Project-02/tester.m
+++ Project-02/tester.m
@@ -5,7 +5,7 @@ clc
 position_begin = [ 3.00 3.00 3.00; ...
                    3.00 6.00 3.00];
 
-position_end   = [ 6.00 5.25 4.00; ...
+position_end   = [ 6.00 5.25 4.50; ...
                    6.00 3.00 6.00];
 
 sensor_ranges   = [ 0.45; 0.50; 0.65 ];
@@ -13,7 +13,7 @@ step_sizes      = [ 0.05; 0.10; 0.15 ];
 clearance       = 10;
 filename        = 'obstacles_1.txt';
 
-for i = 1:length(position_begin)
+for i = 3
     for j = 1:length(sensor_ranges)
         for k = 1:length(step_sizes)
             sensor_range    = sensor_ranges(j);
