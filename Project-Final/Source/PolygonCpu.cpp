b98c9e9e4fab11dbb30a43bb8866980be6372f8b
Andre Kirchner <kirchnerandre@hotmail.com>
2025-05-04 13:27:37 -0700

    test_second_egde_vertical

--- Project-Final/Source/PolygonCpu.cpp
+++ Project-Final/Source/PolygonCpu.cpp
@@ -6,6 +6,24 @@
 
 namespace
 {
+    bool get_coefficients(float& M, float& N, VERTEX_T& VertexA, VERTEX_T& VertexB)
+    {
+        if (VertexA.X == VertexB.X)
+        {
+            return false;
+        }
+        else
+        {
+            M = static_cast<float>(VertexB.Y - VertexA.Y) / static_cast<float>(VertexB.X - VertexA.X);
+
+            N = static_cast<float>(VertexA.Y) - M * static_cast<float>(VertexA.X);
+
+            return true;
+        }
+
+    }
+
+
     bool test_edges_colision(VERTEX_T& VertexA, VERTEX_T& VertexB, VERTEX_T& VertexC, VERTEX_T& VertexD)
     {
         if ((VertexA.X == VertexB.X) && (VertexC.X == VertexD.X))
@@ -46,11 +64,45 @@ namespace
         }
         else if (VertexA.X == VertexB.X)
         {
+            int32_t y_a = VertexA.Y <= VertexB.Y ? VertexA.Y : VertexB.Y;
+            int32_t y_b = VertexA.Y >  VertexB.Y ? VertexA.Y : VertexB.Y;
 
+            float m_cd  = 0.0f;
+            float n_cd  = 0.0f;
+
+            get_coefficients(m_cd, n_cd, VertexC, VertexD);
+
+            float y     = m_cd * VertexA.X + n_cd;
+
+            if ((y_a <= y) && (y <= y_b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else if (VertexC.X == VertexD.X)
         {
+            int32_t y_c = VertexC.Y <= VertexD.Y ? VertexC.Y : VertexD.Y;
+            int32_t y_d = VertexC.Y >  VertexD.Y ? VertexC.Y : VertexD.Y;
 
+            float m_ab  = 0.0f;
+            float n_ab  = 0.0f;
+
+            get_coefficients(m_ab, n_ab, VertexA, VertexB);
+
+            float y     = m_ab * VertexC.X + n_ab;
+
+            if ((y_c <= y) && (y <= y_d))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
@@ -99,7 +151,7 @@ bool test_colision(VERTICES_T& Vertices, int32_t Offset, int32_t Size, EDGE_T& E
             offset_d = Offset + i + 1;
         }
 
-        if (test_edges_colision(Edge.VertexA, Edge.VertexB, Vertices[offset_c], Vertices[offset_d]))
+        if (test_edges_colision(Vertices[offset_c], Vertices[offset_d], Edge.VertexA, Edge.VertexB))
         {
             printf("true\n");
             return true;
--- Project-Final/Tests/TestsPolygon.cpp
+++ Project-Final/Tests/TestsPolygon.cpp
@@ -6,7 +6,7 @@
 
 namespace
 {
-    bool test_egdes_vertical()
+    bool test_both_egdes_vertical()
     {
         VERTICES_T  vertices_a_100_200  = { { 100, 100 }, { 100, 200 } };
         VERTICES_T  vertices_a_200_400  = { { 100, 200 }, { 100, 400 } };
@@ -22,18 +22,6 @@ namespace
 
         EDGE_T      edge                = { { 100, 300 }, { 100, 600 }, false };
 
-        bool        expected_1[]        = { false,
-                                            true,
-                                            true,
-                                            true,
-                                            false };
-
-        bool        expected_2[]        = { false,
-                                            false,
-                                            false,
-                                            false,
-                                            false };
-#if 0
         if (test_colision(vertices_a_100_200, 0, 2, edge) != false)
         {
             fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
@@ -63,7 +51,7 @@ namespace
             fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
             return false;
         }
-#endif
+
         if (test_colision(vertices_b_100_200, 0, 2, edge) != false)
         {
             fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
@@ -96,17 +84,91 @@ namespace
 
         return true;
     }
+
+
+    bool test_first_egde_vertical()
+    {
+        VERTICES_T  vertices_100_200  = { { 100, 200 }, { 100, 400 } };
+
+        EDGE_T      edge_above        = { { 0,   0   }, { 200, 300 }, false };
+        EDGE_T      edge_middle       = { { 0,   300 }, { 200, 300 }, false };
+        EDGE_T      edge_bellow       = { { 0,   600 }, { 200, 300 }, false };
+
+        if (test_colision(vertices_100_200, 0, 2, edge_above) != false)
+        {
+            fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
+            return false;
+        }
+
+        if (test_colision(vertices_100_200, 0, 2, edge_middle) != true)
+        {
+            fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
+            return false;
+        }
+
+        if (test_colision(vertices_100_200, 0, 2, edge_bellow) != false)
+        {
+            fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
+            return false;
+        }
+        return true;
+    }
+
+
+    bool test_second_egde_vertical()
+    {
+        VERTICES_T  vertices_100_200_above  = { { 0,   300 }, { 200, 0   } };
+        VERTICES_T  vertices_100_200_middle = { { 0,   300 }, { 200, 300 } };
+        VERTICES_T  vertices_100_200_bellow = { { 0,   300 }, { 200, 600 } };
+
+        EDGE_T      edge                    = { { 100, 200 }, { 100, 400 }, false };
+
+        if (test_colision(vertices_100_200_above, 0, 2, edge) != false)
+        {
+            fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
+            return false;
+        }
+
+        if (test_colision(vertices_100_200_middle, 0, 2, edge) != true)
+        {
+            fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
+            return false;
+        }
+
+        if (test_colision(vertices_100_200_bellow, 0, 2, edge) != false)
+        {
+            fprintf(stderr, "%s:%d:%s: Incorrect collision test output\n", __FILE__, __LINE__, __FUNCTION__);
+            return false;
+        }
+
+        return true;
+    }
 }
 
 
 int main(int argc, char** argv)
 {
-    if (!test_egdes_vertical())
+#if 0
+    if (!test_both_egdes_vertical())
     {
         fprintf(stderr, "%s:%d:%s: Test failed\n", __FILE__, __LINE__, __FUNCTION__);
         return -1;
     }
-
+#endif
+#if 0
+    if (!test_first_egde_vertical())
+    {
+        fprintf(stderr, "%s:%d:%s: Test failed\n", __FILE__, __LINE__, __FUNCTION__);
+        return -1;
+    }
+#endif
+#if 1
+    if (!test_second_egde_vertical())
+    {
+        fprintf(stderr, "%s:%d:%s: Test failed\n", __FILE__, __LINE__, __FUNCTION__);
+        return -1;
+    }
+#endif
     printf("Helloe owlrd\n");
 
     return 0;
